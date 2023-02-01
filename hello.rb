94fecd26ad6278a3ea63c9df2b55c208e24bc278
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2023-02-01 19:49:16 -0300

    Add file hello.rb and Add operators

---
+++ hello.rb
@@ -0,0 +1,63 @@
+puts '***********************'
+puts '***** Hello Ruby! *****'
+puts '***********************'
+puts ""
+
+puts "************************"
+puts "*Operators / Operadores*"
+puts "************************"
+
+puts ""
+greater_than = 22 > 21 # true
+puts "What's value: greater_than: #{greater_than}"
+puts ""
+
+less_than = 9 < 10 # true
+puts "What's value: less_than: #{less_than}"
+puts ""
+
+greater_or_equal_than = -1 >= 0 # false [falso]
+puts "What's value: greater_or_equal_than: #{greater_or_equal_than}"
+puts ""
+
+less_or_equal_than = -1 <= 0 # true [verdadeiro]
+puts "What's value: less_or_equal_than: #{less_or_equal_than}"
+puts ""
+
+equals = 0 == 0 # true
+puts "What's value: equals: #{equals}"
+puts ""
+
+not_equals = -1 != 0 # true
+puts "What's value: equals: #{equals}"
+puts ""
+
+greater_equal_or_less = 0 <=> -2 # true
+puts "What's value: greater_equal_or_less: #{greater_equal_or_less}"
+puts ""
+
+puts "************************"
+puts "*Arithmetic - Operators *"
+puts "************************"
+
+
+sum = 10 + 20 # 30
+puts "Sum #{sum}"
+
+subtraction = 40 - 20 # 20 
+puts "Substraction #{subtraction}"
+
+division = 10 / 2 # 5
+puts "division #{division}"
+
+multiplication = 10 * 2 # 20
+puts "multiplication #{multiplication}"
+
+power = 10 ** 2 # 100
+puts "power #{power}"
+
+moduleOperator = 6 % 2 # 0 
+moduleOperator2 = 5 % 2 # 1 
+puts "moduleOperator #{moduleOperator}"
+puts "moduleOperator2 #{moduleOperator2}"
+
